b913065f7303786d79b43f8136d666f8fce8858c
llmooon <llmooon@naver.com>
2018-07-20 16:27:28 +0900

    002.add logback settings

--- src/main/java/org/soma/tripper/controller/SampleController.java
+++ src/main/java/org/soma/tripper/controller/SampleController.java
@@ -2,6 +2,8 @@ package org.soma.tripper.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(value="sampleController",description = "샘플 API")
 public class SampleController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/hello")
     @ApiOperation(value="sample api",notes = "테스트 용 API")
     public String helloworld(){
+        logger.info("run helloworld");
         return "hello, world!";
     }
 }
